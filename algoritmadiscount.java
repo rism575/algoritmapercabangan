9087c8c0a874e63ef14ad0faecc117ef2df6773c
rism575 <132285072+rism575@users.noreply.github.com>
2023-10-03 18:01:34 +0800

    Create algoritmadiscount.java

---
+++ algoritmadiscount.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class discount {
+    
+    public static void main(String[] args)throws Exception {
+        Double totalBelanja, discount;
+
+        Scanner inputan = new Scanner(System.in);
+        System.out.print("Masukan total belanja: ");
+        totalBelanja = inputan.nextDouble();
+
+        if (totalBelanja >= 1000000) {
+            discount = 5/100 * totalBelanja;
+            totalBelanja = totalBelanja - discount;
+        }
+        System.out.println("total yang harus dibayar: " + totalBelanja);
+        inputan.close();
+        
+
+    }
+ }
